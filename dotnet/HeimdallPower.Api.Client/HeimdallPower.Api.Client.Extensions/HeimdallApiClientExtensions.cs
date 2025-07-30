7404b16724b9730e35e9abd7d11355a486038ccd
Marius Hjertaker <97670197+mHjertaker@users.noreply.github.com>
2025-07-30 13:43:38 +0200

    feat: add DI extension package for HeimdallApiClient with resilience and options support
    Introduces a new standalone NuGet package:
    
    The package provides an extension method:
      - AddHeimdallPowerApiClient(...)
    
    This simplifies DI registration for `HeimdallApiClient` and configures:
      - A resilient named HttpClient using AddStandardResilienceHandler
      - Token-based client setup using IOptions<HeimdallClientOptions>

---
+++ dotnet/HeimdallPower.Api.Client/HeimdallPower.Api.Client.Extensions/HeimdallApiClientExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace HeimdallPower.Api.Client.Extensions;
+
+/// <summary>
+/// Extension methods for adding the Heimdall Power API client to the service collection.
+/// </summary>
+public static class HeimdallApiClientExtensions
+{
+    /// <summary>
+    /// Adds the Heimdall Power API client to the service collection.
+    /// The client is using standard resilience handlers to handle transient errors.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configureOptions"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddHeimdallPowerApiClient(this IServiceCollection services, Action<HeimdallApiClientOptions> configureOptions)
+    {
+        const string clientName = "HeimdallPower";
+
+        services.Configure(configureOptions);
+
+        services.AddHttpClient(clientName)
+            .ConfigureHttpClient((_, client) =>
+            {
+                client.BaseAddress = new Uri("https://external-api.heimdallcloud.com");
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+            })
+            .AddStandardResilienceHandler();
+
+        services.AddSingleton(sp =>
+        {
+            var options = sp.GetRequiredService<IOptions<HeimdallApiClientOptions>>().Value;
+            var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+            var httpClient = httpClientFactory.CreateClient(clientName);
+
+            return new HeimdallApiClient(options.ClientId, options.ClientSecret, httpClient);
+        });
+
+        return services;
+    }
+}
---
+++ dotnet/HeimdallPower.Api.Client/HeimdallPower.Api.Client.Extensions/HeimdallApiClientOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace HeimdallPower.Api.Client.Extensions;
+
+/// <summary>
+/// Options for configuring the Heimdall Power API client.
+/// </summary>
+public class HeimdallApiClientOptions
+{
+    /// <summary>
+    /// The client ID for the Heimdall Power API.
+    /// </summary>
+    public required string ClientId { get; init; }
+
+    /// <summary>
+    /// The client secret for the Heimdall Power API.
+    /// </summary>
+    public required string ClientSecret { get; init; }
+}
